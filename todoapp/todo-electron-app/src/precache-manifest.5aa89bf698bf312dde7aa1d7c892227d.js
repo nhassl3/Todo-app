c37af51b91ba1b24dc1e77c03f46773a2ebb0dc0
nhassl3 <matvey.kvasov.05@mail.ru>
2024-05-25 23:53:29 +0300

    ✅ Added desktop version of the application

---
+++ todoapp/todo-electron-app/src/precache-manifest.5aa89bf698bf312dde7aa1d7c892227d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9220f1f3b2e8d02c559840e5c7ef805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df873f7d171777ad0da6",
+    "url": "/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "088d379acd4b0c45d555",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "df873f7d171777ad0da6",
+    "url": "/static/js/2.09c410d1.chunk.js"
+  },
+  {
+    "revision": "c8c38381e12ec9a63b77",
+    "url": "/static/js/3.89dfd605.chunk.js"
+  },
+  {
+    "revision": "088d379acd4b0c45d555",
+    "url": "/static/js/main.7cff4b46.chunk.js"
+  },
+  {
+    "revision": "3b50f741942eca7b2099",
+    "url": "/static/js/runtime~main.696eecd9.js"
+  }
+]);
